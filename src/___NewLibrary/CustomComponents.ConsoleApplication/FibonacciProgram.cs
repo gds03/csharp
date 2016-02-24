d484e2fea34eef9f0d593d310955030c22857688
goncalod <goncaloRdias@hotmail.com>
2016-02-24 21:23:34 +0000

    Adding Fibonacci Lazy Calculator

---
+++ src/___NewLibrary/CustomComponents.ConsoleApplication/FibonacciProgram.cs
@@ -0,0 +1,25 @@
+﻿//using CustomComponents.Algorithms.Yield;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace CustomComponents.ConsoleApplication
+//{
+//    class FibonacciProgram
+//    {
+//        public static void Main(String[] args)
+//        {
+//            var numbers = FibonacciGenerator.Generate(300);
+
+//            foreach(var n in numbers)
+//            {
+//                Console.Write(n + ", ");
+
+//            }
+
+//            Console.ReadLine();
+//        }
+//    }
+//}
