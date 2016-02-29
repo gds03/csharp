16d145bb605c0f004c1dbd6390c50f72df25278c
goncalod <goncaloRdias@hotmail.com>
2016-02-29 01:32:48 +0000

    Enhancing Linkedlist Features
    Enhancing Linkedlist Features

--- src/___NewLibrary/CustomComponents.ConsoleApplication/LinkedListProgram.cs
+++ src/___NewLibrary/CustomComponents.ConsoleApplication/LinkedListProgram.cs
@@ -1,85 +1,84 @@
-﻿//using CustomComponents.Algorithms.Collections;
-//using CustomComponents.Algorithms.Collections.Generic;
-//using System;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using CustomComponents.Algorithms.Collections;
+using CustomComponents.Algorithms.Collections.Generic;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace CustomComponents.ConsoleApplication
-//{
-//    class LinkedListProgram
-//    {
-//        public static void Main(String[] args)
-//        {
-//            int[] values = new int[] { 1, 2, 3, 4, 6, 9, 12, 15, 20 };
-//            LinkedList<int> data = new LinkedList<int>(values);
+namespace CustomComponents.ConsoleApplication
+{
+    class LinkedListProgram
+    {
+        public static void Main(String[] args)
+        {
+            int[] values = new int[] { 1, 2, 3, 4, 6, 9, 12, 15, 20 };
+            LinkedList<int> data = new LinkedList<int>(values);
 
-//            Action printData = () =>
-//            {
-//                // test enumerators
-//                foreach (int v in data)
-//                {
-//                    Console.WriteLine(v);
-//                }
-//            };
+            Action printData = () =>
+            {
+                // test enumerators
+                foreach (int v in data)
+                {
+                    Console.WriteLine(v);
+                }
+            };
 
-//            Console.WriteLine("+++++++ collection created");
-//            Console.WriteLine("********* listing the collection");
+            Console.WriteLine("+++++++ collection created");
+            Console.WriteLine("********* listing the collection");
 
-//            // data should be ordered
-//            printData();
+            // data should be ordered
+            printData();
 
-//            int val;
-//            int idx;
+            int val;
+            int idx;
 
-//            Console.WriteLine("********* Removing all elements");
-//            for (idx = values.Length-1; idx >= 0; idx--)
-//            {
-//                val = data.RemoveLast();
-//                Console.WriteLine("Expected to remove {0} : got {1}", values[idx], val);
-//            }
-
-
-//            Console.WriteLine("+++++++++ Expect {0} elements in the collection: Current value {1}", 0, data.Count);
-
-//            val = data.RemoveLast();
-//            Console.WriteLine("Expected to not extract anything since collection is empty; extracted value is: {0}", val);
+            Console.WriteLine("********* Removing all elements");
+            for (idx = values.Length - 1; idx >= 0; idx--)
+            {
+                val = data.RemoveLast().Data;
+                Console.WriteLine("Expected to remove {0} : got {1}", values[idx], val);
+            }
 
 
-//            Console.WriteLine("+++++++ Adding elements to the collection at First");
+            Console.WriteLine("+++++++++ Expect {0} elements in the collection: Current value {1}", 0, data.Count);
 
-//            for (idx = 0; idx < values.Length; idx++)
-//            {
-//                data.AddFirst(values[idx]);
-//            }
-  
-
-//            // we should see data reversed since we put them on head.
-//            printData();
-
-//            for (idx = values.Length - 1; idx >= 0; idx--)
-//            {
-//                val = data.RemoveFirst();
-//                Console.WriteLine("Expected to remove {0} : got {1}", values[idx], val);
-//            }
+            Console.WriteLine("Expected to not extract anything since collection is empty; extracted value is: {0}", data.RemoveLast());
 
 
+            Console.WriteLine("+++++++ Adding elements to the collection at First");
 
-//            data = new LinkedList<int>(values);
-//            printData();
-//            Console.WriteLine("+++++++ collection reverted");
-//            data.Reverse();
+            for (idx = 0; idx < values.Length; idx++)
+            {
+                data.AddFirst(values[idx]);
+            }
 
-//            printData();
+
+            // we should see data reversed since we put them on head.
+            printData();
+
+            for (idx = values.Length - 1; idx >= 0; idx--)
+            {
+                val = data.RemoveFirst().Data; ;
+                Console.WriteLine("Expected to remove {0} : got {1}", values[idx], val);
+            }
 
 
 
-//            Console.ReadLine(); 
+            data = new LinkedList<int>(values);
+            printData();
+            Console.WriteLine("+++++++ collection reverted");
+            data.Reverse();
+
+            printData();
 
 
 
-//        } 
+            Console.ReadLine();
 
-//    }
-//}
+
+
+        }
+
+    }
+}
 
--- src/___NewLibrary/CustomComponents.ConsoleApplication/ThreadPoolProgram.cs
+++ src/___NewLibrary/CustomComponents.ConsoleApplication/ThreadPoolProgram.cs
@@ -1,106 +1,106 @@
-﻿using CustomComponents.Algorithms.Threading;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+﻿//using CustomComponents.Algorithms.Threading;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading;
+//using System.Threading.Tasks;
 
-namespace CustomComponents.ConsoleApplication
-{
-    class ThreadPoolProgram
-    {
-        const int ITERATIONS_FOREACH = 1000000;
-        const int NUM_CALLS_FOREACH = 32;
-        static System.Threading.CountdownEvent cde;
+//namespace CustomComponents.ConsoleApplication
+//{
+//    class ThreadPoolProgram
+//    {
+//        const int ITERATIONS_FOREACH = 1000000;
+//        const int NUM_CALLS_FOREACH = 32;
+//        static System.Threading.CountdownEvent cde;
 
-        static void Main(string[] args)
-        {
-            SimpleThreadPool sThreadPool = new SimpleThreadPool();
+//        static void Main(string[] args)
+//        {
+//            SimpleThreadPool sThreadPool = new SimpleThreadPool();
 
-        WORKLABEL:
-            cde = new System.Threading.CountdownEvent(4 * NUM_CALLS_FOREACH);
+//        WORKLABEL:
+//            cde = new System.Threading.CountdownEvent(4 * NUM_CALLS_FOREACH);
 
-            for (int i = 0; i < NUM_CALLS_FOREACH; i++)
-            {
-                sThreadPool.QueueWorkItem(Work1);
-                sThreadPool.QueueWorkItem(Work2);
-                sThreadPool.QueueWorkItem(Work3);
-                sThreadPool.QueueWorkItem(Work4);
-            }
+//            for (int i = 0; i < NUM_CALLS_FOREACH; i++)
+//            {
+//                sThreadPool.QueueWorkItem(Work1);
+//                sThreadPool.QueueWorkItem(Work2);
+//                sThreadPool.QueueWorkItem(Work3);
+//                sThreadPool.QueueWorkItem(Work4);
+//            }
 
-            while (!cde.IsSet)
-            {
-                Console.WriteLine("BUSY THREADS: {0} ", sThreadPool.BusyThreads);
-                Console.WriteLine("Suspended THREADS: {0}", sThreadPool.SuspendedThreads);
-                Thread.Sleep(1000);
-            }
-
-
-            // cde.Wait();
-            Console.WriteLine("All work is done");
-            Console.ReadLine();
-            Console.WriteLine("BUSY THREADS: {0} ", sThreadPool.BusyThreads);
-            Console.WriteLine("Suspended THREADS: {0}", sThreadPool.SuspendedThreads);
-            Console.ReadLine();
-            goto WORKLABEL;
-        }
+//            while (!cde.IsSet)
+//            {
+//                Console.WriteLine("BUSY THREADS: {0} ", sThreadPool.BusyThreads);
+//                Console.WriteLine("Suspended THREADS: {0}", sThreadPool.SuspendedThreads);
+//                Thread.Sleep(1000);
+//            }
 
 
-        static void Work1()
-        {
-            Console.WriteLine("ThreadID: {0}, WORK1 - STARTED", Thread.CurrentThread.ManagedThreadId);
-            List<object> l = new List<object>();
+//            // cde.Wait();
+//            Console.WriteLine("All work is done");
+//            Console.ReadLine();
+//            Console.WriteLine("BUSY THREADS: {0} ", sThreadPool.BusyThreads);
+//            Console.WriteLine("Suspended THREADS: {0}", sThreadPool.SuspendedThreads);
+//            Console.ReadLine();
+//            goto WORKLABEL;
+//        }
 
-            for (int i = 0; i < ITERATIONS_FOREACH; i++)
-            {
-                l.Add(i);
-            }
-            cde.Signal();
 
-            Console.WriteLine("WORK1 - Finished");
-        }
+//        static void Work1()
+//        {
+//            Console.WriteLine("ThreadID: {0}, WORK1 - STARTED", Thread.CurrentThread.ManagedThreadId);
+//            List<object> l = new List<object>();
 
-        static void Work2()
-        {
-            Console.WriteLine("ThreadID: {0}, WORK2 - STARTED", Thread.CurrentThread.ManagedThreadId);
-            LinkedList<object> l = new LinkedList<object>();
+//            for (int i = 0; i < ITERATIONS_FOREACH; i++)
+//            {
+//                l.Add(i);
+//            }
+//            cde.Signal();
 
-            for (int i = 0; i < ITERATIONS_FOREACH; i++)
-            {
-                l.AddLast(i);
-            }
-            cde.Signal();
+//            Console.WriteLine("WORK1 - Finished");
+//        }
 
-            Console.WriteLine("WORK2 - Finished");
-        }
+//        static void Work2()
+//        {
+//            Console.WriteLine("ThreadID: {0}, WORK2 - STARTED", Thread.CurrentThread.ManagedThreadId);
+//            LinkedList<object> l = new LinkedList<object>();
 
-        static void Work3()
-        {
-            Console.WriteLine("ThreadID: {0}, WORK3 - STARTED", Thread.CurrentThread.ManagedThreadId);
-            Dictionary<int, object> l = new Dictionary<int, object>();
+//            for (int i = 0; i < ITERATIONS_FOREACH; i++)
+//            {
+//                l.AddLast(i);
+//            }
+//            cde.Signal();
 
-            for (int i = 0; i < ITERATIONS_FOREACH; i++)
-            {
-                l.Add(i, new object());
-            }
-            cde.Signal();
+//            Console.WriteLine("WORK2 - Finished");
+//        }
 
-            Console.WriteLine("WORK3 - Finished");
-        }
+//        static void Work3()
+//        {
+//            Console.WriteLine("ThreadID: {0}, WORK3 - STARTED", Thread.CurrentThread.ManagedThreadId);
+//            Dictionary<int, object> l = new Dictionary<int, object>();
 
-        static void Work4()
-        {
-            Console.WriteLine("ThreadID: {0}, WORK4 - STARTED", Thread.CurrentThread.ManagedThreadId);
-            HashSet<int> l = new HashSet<int>();
+//            for (int i = 0; i < ITERATIONS_FOREACH; i++)
+//            {
+//                l.Add(i, new object());
+//            }
+//            cde.Signal();
 
-            for (int i = 0; i < ITERATIONS_FOREACH; i++)
-            {
-                l.Add(i);
-            }
-            cde.Signal();
+//            Console.WriteLine("WORK3 - Finished");
+//        }
 
-            Console.WriteLine("WORK4 - Finished");
-        }
-    }
-}
+//        static void Work4()
+//        {
+//            Console.WriteLine("ThreadID: {0}, WORK4 - STARTED", Thread.CurrentThread.ManagedThreadId);
+//            HashSet<int> l = new HashSet<int>();
+
+//            for (int i = 0; i < ITERATIONS_FOREACH; i++)
+//            {
+//                l.Add(i);
+//            }
+//            cde.Signal();
+
+//            Console.WriteLine("WORK4 - Finished");
+//        }
+//    }
+//}
